12e6a21076ecdb599cb328fc8b2275e48910d594
agysft <hne.gi38194.3@gmail.com>
2024-02-12 21:19:04 +0900

    1st commit for Rev.6 XZ PCB

---
+++ main.c
@@ -0,0 +1,293 @@
+/**
+  Generated Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  Description:
+    This header file provides implementations for driver APIs for all modules selected in the GUI.
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.81.8
+        Device            :  PIC16F1705
+        Driver Version    :  2.00
+*/
+
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+#include "mcc_generated_files/mcc.h"
+
+
+/******** I2C LCD ********/
+#include "mcc_generated_files/examples/i2c_master_example.h"
+#define LCD_ADDR 0x3E
+bool LCD = true;
+char DisplayData[8]="12345678";
+
+int is_I2C_Connected(uint8_t slave_address){
+    int exist_the_lcd = 1;  // 0: exist the LCD, 1: No LCD
+    
+    if ( (RC0_GetValue() | RC1_GetValue()) != 0){ // if no pull-up
+        if( I2C_Open(slave_address) == I2C_NOERR){ 
+            exist_the_lcd = 0;
+        } else {
+            if( I2C_Open(slave_address) == I2C_FAIL ){
+                exist_the_lcd = 1;
+            } else {
+                if( I2C_Open(slave_address) == I2C_BUSY ){
+                    exist_the_lcd = 1;
+                }
+            }
+        }
+    }
+    return exist_the_lcd;
+}
+void writeLCDCommand(char t_command){
+    I2C_Write1ByteRegister(LCD_ADDR, 0x00, t_command );
+    __delay_us(30);     //Instruction Execution Time 14.3-26.3us
+}
+void LCD_Init(){
+    __delay_ms(400);
+    writeLCDCommand(0x38);
+    writeLCDCommand(0x39);
+    writeLCDCommand(0x14);
+    writeLCDCommand(0x70);// contast LSB setting ; 0b0111 xxxx
+    writeLCDCommand(0x56);// 5V=0b010100xx, 3V=0b010101xx,  xx=contrast MSB
+    writeLCDCommand(0x6C);
+    __delay_ms(250);
+    writeLCDCommand(0x38);
+    writeLCDCommand(0x0C);
+    writeLCDCommand(0x01);
+    __delay_us(1100);        //Instruction Execution Time 0.59-1.08ms (550:NG, 600:GOOD)
+}
+void LCD_xy(uint8_t x, uint8_t y){
+    writeLCDCommand(0x80 + 0x40 * y + x);
+}
+void LCD_str2(const char *c) {
+    unsigned char wk;
+    for (int i=0;i<8;i++){
+        wk = c[i];
+        if  (wk == 0x00) break;
+        I2C_Write1ByteRegister(LCD_ADDR, 0x40, wk );
+    }
+    __delay_us(30);
+}
+void LCD_clear(){
+    writeLCDCommand(0x01);
+    __delay_us(1100);        //Instruction Execution Time 0.59-1.08ms (550:NG, 600:GOOD)
+}
+void writeLCDData(char t_data){
+    I2C_Write1ByteRegister(LCD_ADDR, 0x40, t_data );
+    __delay_us(30);     //Instruction Execution Time 14.3-26.3us
+}
+void LCD_SetCG(const char *c){
+    for (int i=0;i<40;i++){
+        writeLCDCommand(0x48+(char)i);
+        writeLCDData(c[i]);
+    }
+}void LCD_Level15R(int a){
+    if (a<0) a=0;
+    if (a>0x000f) a=0x000f;
+    for(int i=0;i<8;i++){
+        if(( a >> 1 ) >= i){
+            DisplayData[i] = 2;
+            if( (( a >> 1 ) == i) && (( a & 1 ) == 0)){
+                DisplayData[i] = DisplayData[i]-1;
+            }
+        } else {
+            DisplayData[i]=0x20;
+        }
+    }
+    if (LCD) { LCD_xy(0,0); LCD_str2( DisplayData ); }
+}
+
+void LCD_Meter15R(int a){
+    if (a<0) a=0;
+    if (a>0x000f) a=0x000f;
+    for(int i=0;i<8;i++){
+        if( ( a >> 1 ) == i){
+            DisplayData[i] = 3;
+            if( ( a & 1 ) == 0 ){
+                DisplayData[i] = DisplayData[i]+1;
+            }
+        } else {
+            DisplayData[i]=5;
+        }
+    }
+    if (LCD) { LCD_xy(0,0); LCD_str2( DisplayData ); }
+}
+/*
+                         Main application
+ */
+void main(void)
+{
+    // initialize the device
+    SYSTEM_Initialize();
+
+    // When using interrupts, you need to set the Global and Peripheral Interrupt Enable bits
+    // Use the following macros to:
+
+    // Enable the Global Interrupts
+    //INTERRUPT_GlobalInterruptEnable();
+
+    // Enable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptEnable();
+
+    // Disable the Global Interrupts
+    //INTERRUPT_GlobalInterruptDisable();
+
+    // Disable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptDisable();
+    
+    __delay_ms(20);
+    if ( is_I2C_Connected(LCD_ADDR) == 0 ) {
+        LCD = true; 
+    } else {
+        LCD = false;
+    }
+
+    if (LCD) LCD_Init();
+    const char CG[]={
+        0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,
+        0x1b,0x1b,0x1b,0x1b,0x1b,0x1b,0x1b,0x1b,
+        0x1b,0x03,0x03,0x03,0x03,0x03,0x03,0x1b,
+        0x1b,0x18,0x18,0x18,0x18,0x18,0x18,0x1b,
+        0x1b,0x00,0x00,0x00,0x00,0x00,0x00,0x1b};
+    LCD_SetCG(CG);
+    __delay_ms(20);
+    if (LCD) { LCD_xy(0,0); LCD_str2("Compact "); }
+    if (LCD) { LCD_xy(0,1); LCD_str2(" PnP n.k"); }
+    __delay_ms(2000);
+    LCD_clear();
+
+    uint16_t convertedValue;
+    uint32_t VDDValue100;
+    int i,j,k;
+    int Laser;
+    char ReadData[8];
+    
+    while (1)
+    {
+        // Add your application code
+        /* Measure own power supply voltage */
+        ADC_Initialize();
+        ADC_SelectChannel(channel_FVRBuffer1); //2.048V
+        ADC_TemperatureAcquisitionDelay();
+        ADC_StartConversion();
+        while(!ADC_IsConversionDone());
+        convertedValue = ADC_GetConversionResult();
+        VDDValue100 = 419430 / convertedValue;    //Not enough memory to do float operations. So, x100
+
+        /* Measure LED Current value */
+        ADC_Initialize();   //required this
+        ADC_SelectChannel(channel_AN7); //channel_AN3=current sensor
+        ADC_StartConversion();
+        while(!ADC_IsConversionDone());
+        convertedValue = ADC_GetConversionResult();
+
+        convertedValue = (VDDValue100 * (uint32_t)convertedValue ) >> 10;
+        /* 
+         * Rev.05 board may remove the display
+         *  as it does not detect well a large voltage drop in the LEDs
+         */
+        sprintf(DisplayData, "%dmA ", convertedValue ); 
+        if (LCD) { LCD_xy(4,1); LCD_str2( DisplayData ); }
+
+        /*
+         * If continuous low-level for 500ms, judge no laser.
+         * Wait for a continuous high-level for 80mS
+         *  = Detects breaks in the data sequence
+         */
+        i = 0;
+        k = 0;
+        Laser = 1;  // Laser-Connected
+        while(i < 80){    //80ms
+            __delay_us(1000);
+            i++;
+            if(RA1 ==0){    // 0 = No laser
+                i=0;
+                k++;
+                if(k>500){
+                    Laser = 0;  // Determine No Laser
+                    break;
+                }
+            }
+        }
+        if(Laser == 1){
+            for(j=0;j<7;j++){
+                ReadData[j]=0;
+                while(RA1==1){} //The edge detect of start-bit
+                __delay_us(7500);
+                for(i=0;i<8;i++){
+                    ReadData[j] = ReadData[j] >> 1 ;
+                    if(RA1==1){
+                        ReadData[j] =  ReadData[j] | 0b10000000 ;
+                    }
+                    __delay_us(5000);
+                }
+            }
+            for(j=0;j<8;j++){
+                DisplayData[j]=0;//NULL;
+            }
+            if ( ReadData[5] > 4) {
+                ReadData[5] = ReadData[5] -5;
+            } else {
+                ReadData[5] = ReadData[5] +5;
+            }
+            if(ReadData[4]==0) ReadData[5] =0;
+            if (LCD) { LCD_clear(); }
+            sprintf(DisplayData, "T=%2d.%1d%cC", ReadData[4], ReadData[5], 0xf2);
+            if (LCD) { LCD_xy(0,0); LCD_str2( DisplayData ); }
+            sprintf(DisplayData, "%2x", ReadData[3]);
+            if (LCD) { LCD_xy(0,1); LCD_str2( DisplayData ); }
+            if(ReadData[3]==0x24){  //0x24=500mW Laser, 0x25=2.5W Laser
+                PWM3DCH=0x2;PWM3DCL=0x40;   //Duty Cycle: 0.89%
+            } else {
+                PWM3DCH=0x1;PWM3DCL=0x0;    //Duty Cycle: 0.39%
+            }
+        }
+        else
+        {
+            /* read the air pressure in pipe */
+            ADC_Initialize();   //required this
+            ADC_SelectChannel(channel_AN2); //channel_AN2=pressure sensor
+            ADC_StartConversion();
+            while(!ADC_IsConversionDone());
+            convertedValue = ADC_GetConversionResult();
+
+            convertedValue = ((VDDValue100 * (uint32_t)convertedValue ) >> 10);
+            LCD_Meter15R((convertedValue-50) >>4);
+            sprintf(DisplayData, "%d", convertedValue ); 
+            if (LCD) { LCD_xy(0,1); LCD_str2( DisplayData ); }
+        }
+    }
+}
+/**
+ End of File
+*/
---
+++ mcc_generated_files/pin_manager.h
@@ -0,0 +1,247 @@
+/**
+  @Generated Pin Manager Header File
+
+  @Company:
+    Microchip Technology Inc.
+
+  @File Name:
+    pin_manager.h
+
+  @Summary:
+    This is the Pin Manager file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  @Description
+    This header file provides APIs for driver for .
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.81.8
+        Device            :  PIC16F1705
+        Driver Version    :  2.11
+    The generated drivers are tested against the following:
+        Compiler          :  XC8 2.36 and above
+        MPLAB 	          :  MPLAB X 6.00	
+*/
+
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+#ifndef PIN_MANAGER_H
+#define PIN_MANAGER_H
+
+/**
+  Section: Included Files
+*/
+
+#include <xc.h>
+
+#define INPUT   1
+#define OUTPUT  0
+
+#define HIGH    1
+#define LOW     0
+
+#define ANALOG      1
+#define DIGITAL     0
+
+#define PULL_UP_ENABLED      1
+#define PULL_UP_DISABLED     0
+
+// get/set channel_AN0 aliases
+#define channel_AN0_TRIS                 TRISAbits.TRISA0
+#define channel_AN0_LAT                  LATAbits.LATA0
+#define channel_AN0_PORT                 PORTAbits.RA0
+#define channel_AN0_WPU                  WPUAbits.WPUA0
+#define channel_AN0_OD                   ODCONAbits.ODA0
+#define channel_AN0_ANS                  ANSELAbits.ANSA0
+#define channel_AN0_SetHigh()            do { LATAbits.LATA0 = 1; } while(0)
+#define channel_AN0_SetLow()             do { LATAbits.LATA0 = 0; } while(0)
+#define channel_AN0_Toggle()             do { LATAbits.LATA0 = ~LATAbits.LATA0; } while(0)
+#define channel_AN0_GetValue()           PORTAbits.RA0
+#define channel_AN0_SetDigitalInput()    do { TRISAbits.TRISA0 = 1; } while(0)
+#define channel_AN0_SetDigitalOutput()   do { TRISAbits.TRISA0 = 0; } while(0)
+#define channel_AN0_SetPullup()          do { WPUAbits.WPUA0 = 1; } while(0)
+#define channel_AN0_ResetPullup()        do { WPUAbits.WPUA0 = 0; } while(0)
+#define channel_AN0_SetPushPull()        do { ODCONAbits.ODA0 = 0; } while(0)
+#define channel_AN0_SetOpenDrain()       do { ODCONAbits.ODA0 = 1; } while(0)
+#define channel_AN0_SetAnalogMode()      do { ANSELAbits.ANSA0 = 1; } while(0)
+#define channel_AN0_SetDigitalMode()     do { ANSELAbits.ANSA0 = 0; } while(0)
+
+// get/set IO_RA1 aliases
+#define IO_RA1_TRIS                 TRISAbits.TRISA1
+#define IO_RA1_LAT                  LATAbits.LATA1
+#define IO_RA1_PORT                 PORTAbits.RA1
+#define IO_RA1_WPU                  WPUAbits.WPUA1
+#define IO_RA1_OD                   ODCONAbits.ODA1
+#define IO_RA1_ANS                  ANSELAbits.ANSA1
+#define IO_RA1_SetHigh()            do { LATAbits.LATA1 = 1; } while(0)
+#define IO_RA1_SetLow()             do { LATAbits.LATA1 = 0; } while(0)
+#define IO_RA1_Toggle()             do { LATAbits.LATA1 = ~LATAbits.LATA1; } while(0)
+#define IO_RA1_GetValue()           PORTAbits.RA1
+#define IO_RA1_SetDigitalInput()    do { TRISAbits.TRISA1 = 1; } while(0)
+#define IO_RA1_SetDigitalOutput()   do { TRISAbits.TRISA1 = 0; } while(0)
+#define IO_RA1_SetPullup()          do { WPUAbits.WPUA1 = 1; } while(0)
+#define IO_RA1_ResetPullup()        do { WPUAbits.WPUA1 = 0; } while(0)
+#define IO_RA1_SetPushPull()        do { ODCONAbits.ODA1 = 0; } while(0)
+#define IO_RA1_SetOpenDrain()       do { ODCONAbits.ODA1 = 1; } while(0)
+#define IO_RA1_SetAnalogMode()      do { ANSELAbits.ANSA1 = 1; } while(0)
+#define IO_RA1_SetDigitalMode()     do { ANSELAbits.ANSA1 = 0; } while(0)
+
+// get/set channel_AN2 aliases
+#define channel_AN2_TRIS                 TRISAbits.TRISA2
+#define channel_AN2_LAT                  LATAbits.LATA2
+#define channel_AN2_PORT                 PORTAbits.RA2
+#define channel_AN2_WPU                  WPUAbits.WPUA2
+#define channel_AN2_OD                   ODCONAbits.ODA2
+#define channel_AN2_ANS                  ANSELAbits.ANSA2
+#define channel_AN2_SetHigh()            do { LATAbits.LATA2 = 1; } while(0)
+#define channel_AN2_SetLow()             do { LATAbits.LATA2 = 0; } while(0)
+#define channel_AN2_Toggle()             do { LATAbits.LATA2 = ~LATAbits.LATA2; } while(0)
+#define channel_AN2_GetValue()           PORTAbits.RA2
+#define channel_AN2_SetDigitalInput()    do { TRISAbits.TRISA2 = 1; } while(0)
+#define channel_AN2_SetDigitalOutput()   do { TRISAbits.TRISA2 = 0; } while(0)
+#define channel_AN2_SetPullup()          do { WPUAbits.WPUA2 = 1; } while(0)
+#define channel_AN2_ResetPullup()        do { WPUAbits.WPUA2 = 0; } while(0)
+#define channel_AN2_SetPushPull()        do { ODCONAbits.ODA2 = 0; } while(0)
+#define channel_AN2_SetOpenDrain()       do { ODCONAbits.ODA2 = 1; } while(0)
+#define channel_AN2_SetAnalogMode()      do { ANSELAbits.ANSA2 = 1; } while(0)
+#define channel_AN2_SetDigitalMode()     do { ANSELAbits.ANSA2 = 0; } while(0)
+
+// get/set RA5 procedures
+#define RA5_SetHigh()            do { LATAbits.LATA5 = 1; } while(0)
+#define RA5_SetLow()             do { LATAbits.LATA5 = 0; } while(0)
+#define RA5_Toggle()             do { LATAbits.LATA5 = ~LATAbits.LATA5; } while(0)
+#define RA5_GetValue()              PORTAbits.RA5
+#define RA5_SetDigitalInput()    do { TRISAbits.TRISA5 = 1; } while(0)
+#define RA5_SetDigitalOutput()   do { TRISAbits.TRISA5 = 0; } while(0)
+#define RA5_SetPullup()             do { WPUAbits.WPUA5 = 1; } while(0)
+#define RA5_ResetPullup()           do { WPUAbits.WPUA5 = 0; } while(0)
+
+// get/set RC0 procedures
+#define RC0_SetHigh()            do { LATCbits.LATC0 = 1; } while(0)
+#define RC0_SetLow()             do { LATCbits.LATC0 = 0; } while(0)
+#define RC0_Toggle()             do { LATCbits.LATC0 = ~LATCbits.LATC0; } while(0)
+#define RC0_GetValue()              PORTCbits.RC0
+#define RC0_SetDigitalInput()    do { TRISCbits.TRISC0 = 1; } while(0)
+#define RC0_SetDigitalOutput()   do { TRISCbits.TRISC0 = 0; } while(0)
+#define RC0_SetPullup()             do { WPUCbits.WPUC0 = 1; } while(0)
+#define RC0_ResetPullup()           do { WPUCbits.WPUC0 = 0; } while(0)
+#define RC0_SetAnalogMode()         do { ANSELCbits.ANSC0 = 1; } while(0)
+#define RC0_SetDigitalMode()        do { ANSELCbits.ANSC0 = 0; } while(0)
+
+// get/set RC1 procedures
+#define RC1_SetHigh()            do { LATCbits.LATC1 = 1; } while(0)
+#define RC1_SetLow()             do { LATCbits.LATC1 = 0; } while(0)
+#define RC1_Toggle()             do { LATCbits.LATC1 = ~LATCbits.LATC1; } while(0)
+#define RC1_GetValue()              PORTCbits.RC1
+#define RC1_SetDigitalInput()    do { TRISCbits.TRISC1 = 1; } while(0)
+#define RC1_SetDigitalOutput()   do { TRISCbits.TRISC1 = 0; } while(0)
+#define RC1_SetPullup()             do { WPUCbits.WPUC1 = 1; } while(0)
+#define RC1_ResetPullup()           do { WPUCbits.WPUC1 = 0; } while(0)
+#define RC1_SetAnalogMode()         do { ANSELCbits.ANSC1 = 1; } while(0)
+#define RC1_SetDigitalMode()        do { ANSELCbits.ANSC1 = 0; } while(0)
+
+// get/set RC2 procedures
+#define RC2_SetHigh()            do { LATCbits.LATC2 = 1; } while(0)
+#define RC2_SetLow()             do { LATCbits.LATC2 = 0; } while(0)
+#define RC2_Toggle()             do { LATCbits.LATC2 = ~LATCbits.LATC2; } while(0)
+#define RC2_GetValue()              PORTCbits.RC2
+#define RC2_SetDigitalInput()    do { TRISCbits.TRISC2 = 1; } while(0)
+#define RC2_SetDigitalOutput()   do { TRISCbits.TRISC2 = 0; } while(0)
+#define RC2_SetPullup()             do { WPUCbits.WPUC2 = 1; } while(0)
+#define RC2_ResetPullup()           do { WPUCbits.WPUC2 = 0; } while(0)
+#define RC2_SetAnalogMode()         do { ANSELCbits.ANSC2 = 1; } while(0)
+#define RC2_SetDigitalMode()        do { ANSELCbits.ANSC2 = 0; } while(0)
+
+// get/set channel_AN7 aliases
+#define channel_AN7_TRIS                 TRISCbits.TRISC3
+#define channel_AN7_LAT                  LATCbits.LATC3
+#define channel_AN7_PORT                 PORTCbits.RC3
+#define channel_AN7_WPU                  WPUCbits.WPUC3
+#define channel_AN7_OD                   ODCONCbits.ODC3
+#define channel_AN7_ANS                  ANSELCbits.ANSC3
+#define channel_AN7_SetHigh()            do { LATCbits.LATC3 = 1; } while(0)
+#define channel_AN7_SetLow()             do { LATCbits.LATC3 = 0; } while(0)
+#define channel_AN7_Toggle()             do { LATCbits.LATC3 = ~LATCbits.LATC3; } while(0)
+#define channel_AN7_GetValue()           PORTCbits.RC3
+#define channel_AN7_SetDigitalInput()    do { TRISCbits.TRISC3 = 1; } while(0)
+#define channel_AN7_SetDigitalOutput()   do { TRISCbits.TRISC3 = 0; } while(0)
+#define channel_AN7_SetPullup()          do { WPUCbits.WPUC3 = 1; } while(0)
+#define channel_AN7_ResetPullup()        do { WPUCbits.WPUC3 = 0; } while(0)
+#define channel_AN7_SetPushPull()        do { ODCONCbits.ODC3 = 0; } while(0)
+#define channel_AN7_SetOpenDrain()       do { ODCONCbits.ODC3 = 1; } while(0)
+#define channel_AN7_SetAnalogMode()      do { ANSELCbits.ANSC3 = 1; } while(0)
+#define channel_AN7_SetDigitalMode()     do { ANSELCbits.ANSC3 = 0; } while(0)
+
+// get/set RC4 procedures
+#define RC4_SetHigh()            do { LATCbits.LATC4 = 1; } while(0)
+#define RC4_SetLow()             do { LATCbits.LATC4 = 0; } while(0)
+#define RC4_Toggle()             do { LATCbits.LATC4 = ~LATCbits.LATC4; } while(0)
+#define RC4_GetValue()              PORTCbits.RC4
+#define RC4_SetDigitalInput()    do { TRISCbits.TRISC4 = 1; } while(0)
+#define RC4_SetDigitalOutput()   do { TRISCbits.TRISC4 = 0; } while(0)
+#define RC4_SetPullup()             do { WPUCbits.WPUC4 = 1; } while(0)
+#define RC4_ResetPullup()           do { WPUCbits.WPUC4 = 0; } while(0)
+#define RC4_SetAnalogMode()         do { ANSELCbits.ANSC4 = 1; } while(0)
+#define RC4_SetDigitalMode()        do { ANSELCbits.ANSC4 = 0; } while(0)
+
+// get/set RC5 procedures
+#define RC5_SetHigh()            do { LATCbits.LATC5 = 1; } while(0)
+#define RC5_SetLow()             do { LATCbits.LATC5 = 0; } while(0)
+#define RC5_Toggle()             do { LATCbits.LATC5 = ~LATCbits.LATC5; } while(0)
+#define RC5_GetValue()              PORTCbits.RC5
+#define RC5_SetDigitalInput()    do { TRISCbits.TRISC5 = 1; } while(0)
+#define RC5_SetDigitalOutput()   do { TRISCbits.TRISC5 = 0; } while(0)
+#define RC5_SetPullup()             do { WPUCbits.WPUC5 = 1; } while(0)
+#define RC5_ResetPullup()           do { WPUCbits.WPUC5 = 0; } while(0)
+#define RC5_SetAnalogMode()         do { ANSELCbits.ANSC5 = 1; } while(0)
+#define RC5_SetDigitalMode()        do { ANSELCbits.ANSC5 = 0; } while(0)
+
+/**
+   @Param
+    none
+   @Returns
+    none
+   @Description
+    GPIO and peripheral I/O initialization
+   @Example
+    PIN_MANAGER_Initialize();
+ */
+void PIN_MANAGER_Initialize (void);
+
+/**
+ * @Param
+    none
+ * @Returns
+    none
+ * @Description
+    Interrupt on Change Handling routine
+ * @Example
+    PIN_MANAGER_IOC();
+ */
+void PIN_MANAGER_IOC(void);
+
+
+
+#endif // PIN_MANAGER_H
+/**
+ End of File
+*/
